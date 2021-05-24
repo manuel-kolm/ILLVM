46cc544f8abae90148c5e211a5bdd2bee81a6552
manuel-kolm <83550123+manuel-kolm@users.noreply.github.com>
2021-05-24 11:23:22 +0200

    Added functions which converts floating point types into IR representation

---
+++ ILLVM.Tests/Misc/LFPHelperTest.cs
@@ -0,0 +1,49 @@
+﻿using ILLVM.Misc;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ILLVM.Tests.Misc {
+    public class LFPHelperTest {
+        [Test]
+        public void Float_ParseCheck_Ok() {
+            Assert.AreEqual("0x4028A4ED40000000", LFPHelper.ConvertFloat2IR(12.322123f));
+        }
+
+        [Test]
+        public void Float_Negative_ParseCheck_Ok() {
+            Assert.AreEqual("0xC028A4ED40000000", LFPHelper.ConvertFloat2IR(-12.322123f));
+        }
+
+        [Test]
+        public void Float_EvenNumber_ParseCheck_Ok() {
+            Assert.AreEqual("1.200000e+01", LFPHelper.ConvertFloat2IR(12f));
+        }
+
+        [Test]
+        public void Float_NegativeEvenNumber_ParseCheck_Ok() {
+            Assert.AreEqual("-1.200000e+01", LFPHelper.ConvertFloat2IR(-12f));
+        }
+
+        [Test]
+        public void Double_ParseCheck_Ok() {
+            Assert.AreEqual("0x4028A4ED4E4C942D", LFPHelper.ConvertDouble2IR(12.322123));
+        }
+
+        [Test]
+        public void Double_Negative_ParseCheck_Ok() {
+            Assert.AreEqual("0xC028A4ED4E4C942D", LFPHelper.ConvertDouble2IR(-12.322123));
+        }
+
+        [Test]
+        public void Double_EvenNumber_ParseCheck_Ok() {
+            Assert.AreEqual("1.200000e+01", LFPHelper.ConvertDouble2IR(12));
+        }
+
+        [Test]
+        public void Double_NegativeEvenNumber_ParseCheck_Ok() {
+            Assert.AreEqual("-1.200000e+01", LFPHelper.ConvertDouble2IR(-12));
+        }
+    }
+}
---
+++ ILLVM/Misc/LFPHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ILLVM.Misc {
+    public static class LFPHelper {
+        /// <summary>
+        /// Converts floating point value into LLVM IR representation.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static string ConvertFloat2IR(float value) {
+            if (Math.Abs(value) < 999999 && value - (long)value == 0) {
+                return ((long)value).ToString("0.000000e+00").Replace(",", ".");
+            }
+            var data = BitConverter.GetBytes((double)value);
+            return "0x" + BitConverter.ToInt64(data).ToString("X");
+        }
+
+        /// <summary>
+        /// Converts double floating point value into LLVM representation.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static string ConvertDouble2IR(double value) {
+            if (Math.Abs(value) < 999999 && value - (long)value == 0) {
+                return ((long)value).ToString("0.000000e+00").Replace(",", ".");
+            }
+            var data = BitConverter.GetBytes(value);
+            return "0x" + BitConverter.ToInt64(data).ToString("X");
+        }
+    }
+}
